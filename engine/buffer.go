45dd37152ac729c912c76f4f6ceb09bb51dd6f5e
t3mm1k <t3mm1k@gmail.com>
2026-09-11 17:32:01 +0700

    fix(engine): fix flush method

--- engine/buffer.go
+++ engine/buffer.go
@@ -56,8 +56,7 @@ func (b *Buffer) Blit(child [][]rune, x, y int) {
 
 func (b *Buffer) Flush() {
 	var builder strings.Builder
-
-	builder.WriteString("\033[H")
+	builder.WriteString("\u001B[H\u001B[2J\u001B[3J")
 
 	for _, row := range b.Data {
 		builder.WriteString(string(row))
