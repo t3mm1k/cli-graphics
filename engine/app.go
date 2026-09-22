0ca6960026a3d92a4f6ff33f4a59c2a7ab02a2cd
t3mm1k <t3mm1k@gmail.com>
2026-09-23 00:31:27 +0700

    feat(engine): add logger

--- engine/app.go
+++ engine/app.go
@@ -20,6 +20,9 @@ type App struct {
 	stopChan chan struct{}
 
 	oldTermState *term.State
+
+	enableLogging bool
+	logFile       *os.File
 }
 
 func NewApp(root Component) *App {
@@ -31,13 +34,29 @@ func NewApp(root Component) *App {
 	}
 }
 
+// SetLogging включает или выключает запись логов в файл app.log
+func (app *App) SetLogging(enable bool) *App {
+	app.enableLogging = enable
+	return app
+}
+
 func (app *App) setTickRate(tickRate time.Duration) {
 	app.tickRate = tickRate
 }
 
 func (a *App) Run() error {
+	if a.enableLogging {
+		f, err := enableFileLogging("app.log")
+		if err != nil {
+			return fmt.Errorf("failed to open app.log: %w", err)
+		}
+		a.logFile = f
+		Log.Info("App started with file logging", "tickRate", a.tickRate)
+	}
+
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
+		Log.Error("Failed to enable raw mode", "err", err)
 		return fmt.Errorf("failed to enable raw mode: %w", err)
 	}
 	a.oldTermState = oldState
@@ -122,6 +141,13 @@ func (a *App) cleanup() {
 	}
 
 	fmt.Print("\u001B[?25h\u001B[2J\u001B[H")
+
+	if a.logFile != nil {
+		Log.Info("App cleanup completed, closing log file")
+		_ = a.logFile.Close()
+		resetLogger()
+		a.logFile = nil
+	}
 }
 
 func (a *App) draw() {
@@ -129,9 +155,7 @@ func (a *App) draw() {
 		return
 	}
 	a.root.Render()
-	if base, ok := a.root.(interface{ Flush() }); ok {
-		base.Flush()
-	} else if bc, ok := a.root.(Container); ok {
-		bc.Flush()
+	if flusher, ok := a.root.(interface{ Flush() }); ok {
+		flusher.Flush()
 	}
 }
